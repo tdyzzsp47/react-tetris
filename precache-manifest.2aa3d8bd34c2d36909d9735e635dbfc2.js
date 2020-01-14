7b05b7d65094ca042d96488e79905737964c2ad9
tdyzzsp47 <tdyzzsp47@gmail.com>
2020-01-15 07:17:12 +0900

    Updates

---
+++ precache-manifest.2aa3d8bd34c2d36909d9735e635dbfc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1fd4ed4d1b29ca6b1b1fe777487d70b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2e0c0b9dea99154394d",
+    "url": "/static/css/main.87e73e80.chunk.css"
+  },
+  {
+    "revision": "c31a7f11d796a7d56499",
+    "url": "/static/js/2.b82318df.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b82318df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b2e0c0b9dea99154394d",
+    "url": "/static/js/main.960cd56e.chunk.js"
+  },
+  {
+    "revision": "7abde0ac5263f0bdbf7d",
+    "url": "/static/js/runtime-main.b98fa3ac.js"
+  }
+]);
