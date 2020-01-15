d1f393575577aa90681e3c18284da330c637caa4
tdyzzsp47 <tdyzzsp47@gmail.com>
2020-01-15 15:35:17 +0900

    fix:パス修正

---
+++ build/precache-manifest.6e3b2b868d715893997b1deb391da05d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffbfe915e0abd10393825259f3f6f379",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f0a20c951cdda7f77ff",
+    "url": "/static/css/main.87e73e80.chunk.css"
+  },
+  {
+    "revision": "c31a7f11d796a7d56499",
+    "url": "/static/js/2.b82318df.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b82318df.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f0a20c951cdda7f77ff",
+    "url": "/static/js/main.45256a1b.chunk.js"
+  },
+  {
+    "revision": "7abde0ac5263f0bdbf7d",
+    "url": "/static/js/runtime-main.b98fa3ac.js"
+  }
+]);
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+// pathとかpluginとか
+const path = require('path')
+
+module.exports = {
+    output: {
+        publicPath: '/react-tetris'
+    }
+}
